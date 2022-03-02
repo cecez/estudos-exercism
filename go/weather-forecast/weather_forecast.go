1f0e0bed968444e18a4e1b21d4e324e66897a238
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-02 10:03:03 -0300

    chore: go - weather-forecast - final solution

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather Oferece previsão do tempo atual para uma localização.
 package weather
 
+// CurrentCondition Armazena a condição atual.
 var CurrentCondition string
+// CurrentLocation Armazena a localização atual.
 var CurrentLocation string
 
+// Forecast Imprime dados meteorológicos para uma localização e condição.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
