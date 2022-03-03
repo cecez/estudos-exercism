85a69aa164c1f84904b6db626ce08f3379413361
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-03 15:24:49 -0300

    chore: typescript - resistor-color-trio - solution

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,29 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+enum BandColors {
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+}
+
+type BandColorType = keyof typeof BandColors
+
+export function decodedResistorValue([firstBand, secondBand, thirdBand]: BandColorType[]): string {
+  
+  const initialBandValue: number = +`${BandColors[firstBand]}${BandColors[secondBand]}`
+  const multiplier: number = Math.pow(10, BandColors[thirdBand])
+  let finalBandValue: number = initialBandValue * multiplier
+
+  let prefix = ""
+  if (finalBandValue >= 1000) {
+    finalBandValue = finalBandValue / 1000
+    prefix = "kilo"
+  }
+
+  return `${finalBandValue} ${prefix}ohms`
 }
