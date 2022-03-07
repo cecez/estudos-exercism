b3d6b84510fb936300152da251ef7ccabf4bd2e5
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-07 17:20:22 -0300

    chore: typescript - leap - solution 2

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,5 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number): boolean {
+  var data = new Date(year, 1, 28);
+  data.setDate(data.getDate() + 1);
+  return data.getDate() === 29;
 }
