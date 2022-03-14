d87ab37510cf36e28a14f14c09236bf76ec20393
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-14 15:22:51 -0300

    chore: typescript - rna-transcription - solution

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,17 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+interface dnaReplacement {
+  [index: string]: string
+}
+
+export function toRna(dna: string): string {
+  if (isInvalidDna(dna)) {
+    throw new Error("Invalid input DNA.");
+  } 
+  
+  const replacement: dnaReplacement = {"A":"U", "C":"G", "G": "C", "T": "A"};
+  return dna.replace(/[ACTG]/g, m => replacement[m]);
+}
+
+function isInvalidDna(dna: string): boolean {
+  const regex = /^[ACTG]+$/gm;
+  return !regex.test(dna);
 }
