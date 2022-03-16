a75ef941f7086d05ea414dec729405c1fbdc9c2f
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-16 18:56:37 -0300

    chore: python - currency-exchange - solution

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -1,3 +1,7 @@
+"""
+Currency Exchange methods.
+"""
+
 def exchange_money(budget, exchange_rate):
     """
 
@@ -6,7 +10,7 @@ def exchange_money(budget, exchange_rate):
     :return: float - exchanged value of the foreign currency you can receive.
     """
 
-    pass
+    return budget / exchange_rate
 
 
 def get_change(budget, exchanging_value):
@@ -17,7 +21,7 @@ def get_change(budget, exchanging_value):
     :return: float - amount left of your starting currency after exchanging.
     """
 
-    pass
+    return budget - exchanging_value
 
 
 def get_value_of_bills(denomination, number_of_bills):
@@ -28,7 +32,7 @@ def get_value_of_bills(denomination, number_of_bills):
     :return: int - total value of bills you now have.
     """
 
-    pass
+    return number_of_bills * denomination
 
 
 def get_number_of_bills(budget, denomination):
@@ -39,7 +43,7 @@ def get_number_of_bills(budget, denomination):
     :return: int - number of bills after exchanging all your money.
     """
 
-    pass
+    return budget // denomination
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -52,7 +56,11 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     :return: int - maximum value you can get.
     """
 
-    pass
+    spread_percentage = spread / 100
+    recalculated_rate = exchange_rate + (spread_percentage * exchange_rate)
+    adjusted_budget = exchange_money(budget, recalculated_rate)
+    number_of_bills = get_number_of_bills(adjusted_budget, denomination)
+    return get_value_of_bills(denomination, number_of_bills)
 
 
 def non_exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -65,4 +73,8 @@ def non_exchangeable_value(budget, exchange_rate, spread, denomination):
     :return: int non-exchangeable value.
     """
 
-    pass
+    spread_percentage = spread / 100
+    recalculated_rate = exchange_rate + (spread_percentage * exchange_rate)
+    adjusted_budget = exchange_money(budget, recalculated_rate)
+    e_value = exchangeable_value(budget, exchange_rate, spread, denomination)
+    return int(adjusted_budget - e_value)
