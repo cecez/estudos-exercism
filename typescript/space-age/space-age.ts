078f8fb0217ae71371b5bdb7f9e99bc12c6b060d
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-21 14:23:26 -0300

    chore: typescript - space-age - solution

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,18 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const oneEarthYearInSeconds = 31557600;
+
+enum Planets {
+  "mercury" = 0.2408467,
+  "venus" = 0.61519726,
+  "earth" = 1,
+  "mars" = 1.8808158,
+  "jupiter" = 11.862615,
+  "saturn" = 29.447498,
+  "uranus" = 84.016846,
+  "neptune" = 164.79132
+}
+
+type PlanetsType = keyof typeof Planets
+
+export function age(planet: PlanetsType, seconds: number): number {
+  return Number((seconds / (oneEarthYearInSeconds * Planets[planet])).toFixed(2));
 }
