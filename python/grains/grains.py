b9bab85467b7e8f370e88530ba1f9d5ae4a3e133
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-23 19:20:29 -0300

    chore: python - grains - solution

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,17 @@
 def square(number):
-    pass
+    """
+    Calculate the number of grains on a given square.
+    :param number: square
+    :return: number
+    """
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    return pow(2, number - 1)
 
 
 def total():
-    pass
+    """
+    Calculate the total number of grains on the chessboard. (64 squares)
+    :return: number
+    """
+    return pow(2, 64) - 1
