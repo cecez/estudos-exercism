4a3a1e7f7cb484b3bfc93b108823e1e4d9549b51
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-24 12:01:04 -0300

    chore: swift - lasagna - solution

--- swift/lasagna/Tests/LasagnaTests/LasagnaTests.swift
+++ swift/lasagna/Tests/LasagnaTests/LasagnaTests.swift
@@ -9,17 +9,17 @@ final class LasagnaTests: XCTestCase {
   }
 
   func testRemainingMinutes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    try XCTSkipIf(false && !runAll)  // change true to false to run this test
     XCTAssertEqual(remainingMinutesInOven(elapsedMinutes: 13), 27)
   }
 
   func testPreperationMinutes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    try XCTSkipIf(false && !runAll)  // change true to false to run this test
     XCTAssertEqual(preparationTimeInMinutes(layers: 6), 12)
   }
 
   func testTotalMinutes() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    try XCTSkipIf(false && !runAll)  // change true to false to run this test
     XCTAssertEqual(totalTimeInMinutes(layers: 6, elapsedMinutes: 13), 25)
   }
 
