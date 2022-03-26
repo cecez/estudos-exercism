067df4603a4d9283049a2f594d7e7cff7e0e998d
Cezar Rosa <cezar@nitrodev.com>
2022-03-26 11:14:43 -0300

    chore: java - micro-blog - first solution

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -1,5 +1,12 @@
 class MicroBlog {
     public String truncate(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] arr = input.replaceAll("\\p{So}|.", "$0\0").split("\0+");
+
+        StringBuilder output = new StringBuilder("");
+        for (int i=0; i < Math.min(5, arr.length); i++) {
+            output.append(arr[i]);
+        }
+
+        return output.toString();
     }
 }
