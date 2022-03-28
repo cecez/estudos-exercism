aa47bb837ddeaf60309ad3c4b6c329d207d78a77
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-28 18:56:27 -0300

    chore: typescript - dnd-character - initial solution

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,39 @@
+function rollTheDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+// type AbilityScore = 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18;
+
 export class DnDCharacter {
+  charisma: number;
+  constitution: number;
+  dexterity: number;
+  hitpoints: number;
+  intelligence: number;
+  strength: number;
+  wisdom: number;
+
+  constructor() {
+    this.charisma = DnDCharacter.generateAbilityScore()
+    this.constitution = DnDCharacter.generateAbilityScore()
+    this.dexterity = DnDCharacter.generateAbilityScore()
+    this.intelligence = DnDCharacter.generateAbilityScore()
+    this.strength = DnDCharacter.generateAbilityScore()
+    this.wisdom = DnDCharacter.generateAbilityScore()
+
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution)
+  }
+
+
   public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+    const dices: number[] = [rollTheDice(), rollTheDice(), rollTheDice(), rollTheDice()];
+    dices.sort();
+    const a = dices.splice(1);
+    return a.reduce((a, b) => a + b, 0);
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+    return Math.floor((abilityValue - 10) / 2);
   }
+
 }
