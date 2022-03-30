69e0f009b8568a0595d5f7591b37742067e275d0
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-30 18:53:01 -0300

    chore: python - darts - initial solution

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,17 @@
+from math import sqrt
+
+
 def score(x, y):
-    pass
+    """
+    Determine the score for a dart thrown at (x, y).
+    """
+
+    radius = sqrt(x ** 2 + y ** 2)
+    if radius <= 1:
+        return 10
+    elif radius <= 5:
+        return 5
+    elif radius <= 10:
+        return 1
+    else:
+        return 0
