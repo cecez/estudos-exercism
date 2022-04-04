d8a9675afa62f3309ea19d0d10f33ed0846e89f4
Cezar Castro Rosa <cecez86@gmail.com>
2022-04-04 19:00:36 -0300

    chore: typescript - pangram - solution

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,3 +1,11 @@
-export function isPangram() {
-  throw new Error('Remove this statement and implement this function')
+export function isPangram(sentence: string): boolean {
+  // const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  // const letters = sentence.toLowerCase().split('')
+  // return alphabet.split('').every(letter => letters.includes(letter))
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const sentenceLetters = sentence.toLowerCase().replace(/[^a-z]+/g, "").split("").sort()
+  const uniqueLetters = [...new Set(sentenceLetters)].join("")
+
+  return alphabet === uniqueLetters
 }
