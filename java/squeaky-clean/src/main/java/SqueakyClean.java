8021e47f5895039e2884e3f8dd281b5f82955147
Cezar Rosa <cecez86@gmail.com>
2022-04-09 20:03:18 -0300

    chore: java - squeaky-clean - solution

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,37 @@
+import java.util.regex.Pattern;
+
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        char[] caracteres = identifier.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean convertToUpper = false;
+        for (Character caractere : caracteres) {
+
+            if (Pattern.matches("[α-ω]", caractere.toString())) {
+                continue;
+            }
+
+            if (Pattern.matches("[-]", caractere.toString())) {
+                convertToUpper = true;
+                continue;
+            }
+
+            if (Character.isWhitespace(caractere)) {
+                stringBuilder.append("_");
+            } else if (Character.isISOControl(caractere)) {
+                stringBuilder.append("CTRL");
+            } else if (Character.isLetter(caractere)) {
+
+                if (convertToUpper) {
+                    stringBuilder.append(Character.toUpperCase(caractere));
+                    convertToUpper = false;
+                } else {
+                    stringBuilder.append(caractere);
+                }
+
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
