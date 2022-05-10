48750154f3116dcd98876a8cb89579e8244c443c
Cezar Rosa <cecez86@gmail.com>
2022-05-09 22:35:15 -0300

    chore: java - cars-assemble - solution

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,25 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+        double successRate = this.successRate(speed);
+        int carsProducedPerHour = 221;
+
+        return speed * carsProducedPerHour * successRate;
+    }
+
+    private double successRate(int speed) {
+        return switch (speed) {
+            case 1, 2, 3, 4 -> 1;
+            case 5, 6, 7, 8 -> 0.9;
+            case 9 -> 0.8;
+            case 10 -> 0.77;
+            default -> 0.0;
+        };
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        double carsProducedPerMinute = this.productionRatePerHour(speed) / 60;
+        return (int) carsProducedPerMinute;
     }
+ 
 }
