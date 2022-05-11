6c7787dea6e855d59f5a2c6971c7f8b2206d5834
Cezar Rosa <cecez86@gmail.com>
2022-05-10 21:32:27 -0300

    chore: java - raindrops - solution

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,27 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder output = new StringBuilder();
+
+        boolean isDivisibleBy3 = number % 3 == 0;
+        boolean isDivisibleBy5 = number % 5 == 0;
+        boolean isDivisibleBy7 = number % 7 == 0;
+
+        if (isDivisibleBy3) {
+            output.append("Pling");
+        }
+        if (isDivisibleBy5) {
+            output.append("Plang");
+        }
+        if (isDivisibleBy7) {
+            output.append("Plong");
+        }
+
+        if (!isDivisibleBy3 && !isDivisibleBy5 && !isDivisibleBy7) {
+            output.append(number);
+        }
+
+        return output.toString();
     }
 
 }
