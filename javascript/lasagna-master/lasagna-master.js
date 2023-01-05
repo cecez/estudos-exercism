e95df7793605ba8b3888e82d53eb6c3bcf74e8bb
Cezar Castro Rosa <cecez86@gmail.com>
2023-01-05 18:10:59 -0300

    exercism solucao js lasagna-master

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,69 @@
  *   ...
  * }
  */
+
+/**
+ *
+ * @param {number?} remainingTime time in minutes.
+ * @returns {string} description
+ */
+export function cookingStatus(remainingTime) {
+  if (remainingTime == 0) {
+    return "Lasagna is done.";
+  }
+
+  if (Number(remainingTime)) {
+    return "Not done, please wait.";
+  }
+
+  return "You forgot to set the timer.";
+}
+
+/**
+ *
+ * @param {string[]} layers Lasagna layers.
+ * @param {number} average Average preparation time per layer in minutes
+ * @returns {number} Estimation of preparation time.
+ */
+export function preparationTime(layers, average = 2) {
+  return layers.length * average;
+}
+
+/**
+ *
+ * @param {string[]} layers Lasagna layers.
+ * @returns {object}
+ */
+export function quantities(layers) {
+  const sauceLayers = layers.filter((layer) => layer === "sauce");
+  const noodleLayers = layers.filter((layer) => layer === "noodles");
+  return {
+    noodles: noodleLayers.length * 50,
+    sauce: sauceLayers.length * 0.2,
+  };
+}
+
+/**
+ *
+ * @param {string[]} friendIngredientList
+ * @param {string[]} myIngredientList
+ */
+export function addSecretIngredient(friendIngredientList, myIngredientList) {
+  const secretIngredient =
+    friendIngredientList[friendIngredientList.length - 1];
+  myIngredientList.push(secretIngredient);
+}
+
+/**
+ *
+ * @param {{noodles: number, sauce: number, mozzarella: number, meat: number} | {}} recipe Recipe for two portions.
+ * @param {number} numberOfPortions
+ */
+export function scaleRecipe(recipe, numberOfPortions) {
+    const scaledRecipe = {};
+    const multiplier = numberOfPortions / 2;
+    for (const key in recipe) {
+        scaledRecipe[key] = multiplier * recipe[key];
+    }
+    return scaledRecipe;
+}
